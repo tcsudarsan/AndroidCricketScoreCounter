6ad0277928c0b79fe0642b0a7f04e25e5099c520
tcsudarsan <tcsudarsan@gmail.com>
2017-07-26 19:34:30 +0200

    TeamB.java

---
+++ TeamB.java
@@ -0,0 +1,13 @@
+package com.example.sudarsan.criccounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_b);
+    }
+}
